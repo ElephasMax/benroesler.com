f5826bee766a411488876df9eb635af2e1e6aa48
Benny Roesler <bennytroesler@gmail.com>
2022-03-16 21:16:45 -0600

    added PGP key

--- landing/urls.py
+++ landing/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.home, name="landing-home"),
+    path('pgp', views.pgp, name="pgp-view ")
 ]
--- landing/views.py
+++ landing/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,12 +6,16 @@ def home(request):
     context = {        
         'title' : 'Ben Roesler',
         'email' : 'bennytroesler@gmail.com',
+        'pgpURL' : "https://benroesler.com/pgp",
         'phone' : '(847) 571-2749',
         'name'  : 'Ben Roesler',
 
         'core' : [
-            { 'https://drive.google.com/file/d/1FdoueBrdZfk7RPF5BhOaMXXhZbCQpRCe/view?usp=sharing' : 'Resume' },
+            { 'https://drive.google.com/file/d/10ft-feIjkUkVU63L45lR-2oS0qATKSke/view?usp=sharing' : 'Resume' },
             { 'https://drive.google.com/drive/folders/146NSwf5Le2YLQEpSQfj7ZnaJ8irOrgW-?usp=sharing' : 'Certifications'},
             { 'https://www.linkedin.com/in/bennyroesler/' : 'Linkedin' },
         ],}
     return render(request,'landing/landing.html', context) 
+
+def pgp(request):    
+    return HttpResponse(open("landing/static/files/Ben_bennytroesler@gmail.com-0x90E8D08375659A63-pub.asc", 'r'),content_type="text/plain")
