577b9acbf4df0dbaa5499f8c1813cf9732f8004e
Benny Roesler <bennytroesler@gmail.com>
2022-03-19 22:25:41 -0600

    added overwrite of static files

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 #! /bin/bash
 
 cd /opt/benroesler.com
-# pip install --no-cache-dir -r requirements.txt
-
-# pip install -r requirements.txt
-python manage.py collectstatic; sleep 10
+python manage.py collectstatic -c --noinput
 gunicorn --workers=2 --bind=0.0.0.0:8000 benroesler.wsgi
