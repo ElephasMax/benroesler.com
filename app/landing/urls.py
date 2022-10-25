8e2d3c916cec5fafdba3126ecb4e0100f6675144
Benny Roesler <bennytroesler@gmail.com>
2022-10-25 01:36:49 -0600

    updated PGP key location

--- app/landing/urls.py
+++ app/landing/urls.py
@@ -2,5 +2,4 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.home, name="landing-home"),
-    path('pgp', views.pgp, name="pgp-view ")
 ]
--- app/landing/views.py
+++ app/landing/views.py
@@ -7,7 +7,7 @@ def home(request):
         'title' : 'Benny Roesler',
         'subtitle' : 'Security Researcher, Hack The Box Enthusiast, Adventurer',
         'email' : 'bennytroesler@gmail.com',
-        'pgpURL' : "https://benroesler.com/pgp",
+        'pgpURL' : "https://files.benroesler.com/c/Ben_bennytroesler@gmail.com-0x90E8D08375659A63-pub.asc",
         'phone' : '(847) 571-2749',
         'name'  : 'Benny Roesler',
 
@@ -16,6 +16,3 @@ def home(request):
             { 'https://files.benroesler.com/c/certs/' : 'Certifications'},
         ],}
     return render(request,'landing/landing.html', context) 
-
-def pgp(request):    
-    return HttpResponse(open("landing/static/files/Ben_bennytroesler@gmail.com-0x90E8D08375659A63-pub.asc", 'r'),content_type="text/plain")
